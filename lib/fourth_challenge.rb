d18f6a2abfde05951a08a4029ec890839106212f
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-13 14:01:51 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Steven", age: "30", attitude: "Confused"}, {name: "Benvolio" age:"17", attitude:"hot-headed"}
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "Confused"}, {name: "Nurse" age:"44", attitude:"worried"}
         #your hashes here!
       ]
    }
